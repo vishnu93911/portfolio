4332f8a873990b9376e34d9cb14fd9762dace7c4
vishnu93911 <vishhnu040@gmail.com>
2026-03-30 10:09:18 +0530

    Updated hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,6 +30,7 @@ const Hero = () => {
               words={[
                 "Python Full Stack Developer",
                 "Django Developer",
+                "python Developer",
                 "web developer",
                 "Backend Developer",
               ]}
